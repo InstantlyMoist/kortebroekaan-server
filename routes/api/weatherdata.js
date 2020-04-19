bdb035cec4cd00e33680bcf8cb85e3cc3b8f8944
InstantlyMoist <kyllian007@gmail.com>
2020-04-19 13:15:32 +0200

    Final heroku fix

--- routes/api/weatherdata.js
+++ routes/api/weatherdata.js
@@ -18,24 +18,20 @@ router.get("/", async (req, res) => {
     let closestProvince = locationhandler.getClosestProvince(location);
     let result = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${closestProvince.id}&appid=${key}&units=metric`);
     let body = await (result.json());
-    let compiledWeatherData = { test: "hello"};
+    let compiledWeatherData = {};
     for (let index in body.list) {
         let weatherItem = body.list[index];
         let foundDate = new Date(weatherItem.dt * 1000);
-        console.log(foundDate.getWeekDay());
-        console.log(foundDate.getHours());
-        if (compiledWeatherData[foundDate.getWeekDay()] == null && foundDate.getHours() == 14) {
+        if (compiledWeatherData[foundDate.getWeekDay()] == null && (foundDate.getHours() == 14) || foundDate.getHours == 15) {
             let weatherDescription = weatherItem.weather[0]['main'];
             let rain = (weatherDescription == "Drizzle" || weatherDescription == "Thunderstorm" || weatherDescription == "Rain" || weatherDescription == "Snow");
             let object = {
                 temp: weatherItem.main['feels_like'],
                 chanceOfRain: rain
             }
-            console.log(object);
             compiledWeatherData[foundDate.getWeekDay()] = object;
         }
     }
-    console.log(compiledWeatherData);
     res.status(200).json(compiledWeatherData);
 });
 
